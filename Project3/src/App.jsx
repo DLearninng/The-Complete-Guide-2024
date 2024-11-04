cbd0be07dd80c305ba3d142838d43746d34bf7d0
Huy Dau <65066673+HuyDau@users.noreply.github.com>
2024-11-04 17:50:36 +0700

    Components, Jsx, Props, State P1

--- Project3/src/App.jsx
+++ Project3/src/App.jsx
@@ -1,16 +1,42 @@
+import { useState } from 'react';
+import { CORE_CONCEPTS, EXAMPLES } from './data'
+import Header from './components/Header/Header'
+import CoreConcept from './components/CoreConcept/CoreConcept';
+import TabButton from './components/TabButton/TabButton';
+
 function App() {
-  return (
+  const [selectedTopic, setSelectedTopic] = useState('compoents')
+  function handleClick(name) {
+    setSelectedTopic(name)
+  }
+  return ( 
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((_core, index) => <CoreConcept {...CORE_CONCEPTS[index]} />)}
+          </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton label={'Compoents'} onSelect={() => handleClick('compoents')} />
+            <TabButton label={'Jsx'} onSelect={() => handleClick('jsx')} />
+            <TabButton label={'Props'} onSelect={() => handleClick('props')} />
+            <TabButton label={'State'} onSelect={() => handleClick('state')} />
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES.selectedTopic.title}</h3>
+            <p>{EXAMPLES.selectedTopic.description}</p>
+            <pre>
+              <code>
+                {EXAMPLES.selectedTopic.code}
+              </code>
+            </pre>
+          </div>
+        </section>
       </main>
     </div>
   );
